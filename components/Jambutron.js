5e41ee33cf9242b3c760c666ae1053b734004255
David Bobboi Yakubu <55327588+webex001@users.noreply.github.com>
2022-01-27 01:09:17 +0100

    hero section complete

---
+++ components/Jambutron.js
@@ -0,0 +1,18 @@
+import Image from "next/image"
+
+
+function Jambutron() {
+    return (
+        <div className="flex  flex-nowrap justify-center w-9/12 rounded-xl bg-gray-100 pt-32 pb-32 z- -mt-52">
+            <div className="">
+                <h6>Introducing</h6>
+                <h1>Airbnb <br /> gift gards</h1>
+            </div>
+            <div>
+                <img src="/card.png"/>
+            </div>
+        </div>
+    )
+}
+
+export default Jambutron
